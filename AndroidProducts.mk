e540cb174b1c024a7a14e7bb2995e4e34a320b5a
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 21:45:05 +0000

    Adapt to LMODroid by device-add script
    Change-Id: Id1cdfee39afed3ba1fd66f20feca3a1dd475934b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,8 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_barbet.mk \
-    $(LOCAL_DIR)/lineage_barbet.mk
+    $(LOCAL_DIR)/lmodroid_barbet.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_barbet-userdebug \
-    lineage_barbet-userdebug
+    lmodroid_barbet-userdebug
