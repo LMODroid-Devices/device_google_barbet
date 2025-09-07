f0889ed555221dab34fd6dd7bdbede8cca4dec0d
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:41:42 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,8 +17,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_barbet.mk \
     $(LOCAL_DIR)/aosp_barbet_hwasan.mk \
-    $(LOCAL_DIR)/lineage_barbet.mk
+    $(LOCAL_DIR)/lmodroid_barbet.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_barbet-trunk_staging-userdebug \
-    lineage_barbet-trunk_staging-userdebug
+    lmodroid_barbet-trunk_staging-userdebug
--- lmodroid_barbet.mk
+++ lmodroid_barbet.mk
@@ -5,18 +5,18 @@
 #
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/barbet/aosp_barbet.mk)
-$(call inherit-product, device/google/redbull/lineage_common.mk)
+$(call inherit-product, device/google/redbull/lmodroid_common.mk)
 
 include device/google/barbet/device-lineage.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 5a
-PRODUCT_NAME := lineage_barbet
+PRODUCT_NAME := lmodroid_barbet
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
